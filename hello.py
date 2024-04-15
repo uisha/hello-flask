fb34b0be7290a5e0ccddc1e1cf7148dbf4cb58b0
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-04-16 03:07:00 +0800

    using decorators for html rendering

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+
 @app.route('/')
 def hello_world():
     return "<h1>Hello, World!</h1>"
@@ -8,6 +15,11 @@ def hello_world():
 @app.route("/<name>")
 def freet(name):
     return f"Hello there {name}!"
+
+@app.route("/bye")
+@make_bold
+def bye():
+    return "Bye!"
 
 if __name__ == "__main__":
     #execute only if run as a script
