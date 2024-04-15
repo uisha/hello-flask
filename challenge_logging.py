a6b09f583af0743523d735473f8f781db5e3aafc
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2024-04-16 03:22:17 +0800

    Solution for logging_challenge

--- challenge_logging.py
+++ challenge_logging.py
@@ -2,9 +2,9 @@ inputs = eval(input())
 
 def logging_decorator(function):
     def wrapper(*args):
-        print(f"You called {function.__name__}({args[0]}, {args[1]}, {args[2]})")
-        answer = function(args[0], args[1], args[2])
-        print(f"It returned: {answer}")
+        print(f"You called {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
+        print(f"It returned: {result}")
     return wrapper
 
 
